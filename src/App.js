d3db84188ce31049944365d75e1578e9b2af0e53
greatkene <ihevuemeg@gmail.com>
2022-03-23 19:28:54 +0100

    first comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
-function App() {
+const url = 'https://api.github.com/users';
+
+const App = () => {
+
+  const [users, setUsers] = useState([])
+
+ const getUsers = async () => {
+  const response = await fetch(url);
+  const users = await response.json();
+  // console.log(users)
+  setUsers(users)
+ }
+
+  useEffect(()=> {
+    getUsers()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h3>Github Users</h3>
+      <ul className='users'>
+        {
+          users.map((user) => {
+            const {id, login, avatar_url, html_url} = user
+            return <li key={id}>
+            <img src={avatar_url} alt={login}/>
+            <div>
+              <h4>{login}</h4>
+              <a href={html_url}>Profile</a>
+            </div>
+            </li>
+          })
+        }
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
